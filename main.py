8d1e7d872765f3e652d1595b64c1f2b1f50661b6
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-11-09 14:56:25 +0100

    Adding Bitio & Test buttons

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import microbit
-import pyautogui
+import lib
+import time
+
+lib.initCountdown(5)
+
+print("\nmicro:bit connected\n")
+
 while True:
-    if microbit.button_a.was_pressed():
-        pyautogui.click()
+    time.sleep(0.25)
+    if microbit.button_a.was_pressed():  # type: ignore
+        print("Button A pressed")
+        time.sleep(0.5)
+    if microbit.button_b.was_pressed():  # type: ignore
+        print("Button B pressed")
+        time.sleep(0.5)
